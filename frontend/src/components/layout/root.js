d6a22603eeca8a72c3cedda6c509c2635f099898
Sean O'Brien <sean@cyberclear.cc>
2025-01-06 20:34:34 -0500

    Updated footer links

--- frontend/src/components/layout/root.js
+++ frontend/src/components/layout/root.js
@@ -2,13 +2,13 @@ import { Outlet } from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <footer className="fixed bottom-0 left-0 w-full bg-[#0f0f0f] text-white py-2">
+        <footer className="fixed bottom-0 left-0 w-full bg-[#000d1d] text-white py-2">
             <div className="container mx-auto text-center">
                 <p className="text-sm">
-                    Coded with <span className="text-red-500">&lt;3</span> by Kaliendo, available on{' '}
-                    <a href="https://github.com/Kaliendo/aethernote" className="text-blue-400 underline">
-                        GitHub
-                    </a>.
+                    <a href="https:\/\/psafe.ly/vgZeqv" target="_blank" style={{color: '#6ec1e4'}}>Source Code</a>
+                    <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/bugs" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Found A Bug?</a>
+                    <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/abuse" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Report Abuse</a>
+                    <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/VNuFWf" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Terms of Service</a>
                 </p>
             </div>
         </footer>
