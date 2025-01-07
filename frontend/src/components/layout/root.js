ebbec0b6b20f09e755c3318fd9bf4939763cca89
Sean O'Brien <sean@cyberclear.cc>
2025-01-06 23:27:01 -0500

    Added Tor hidden service to footer

--- frontend/src/components/layout/root.js
+++ frontend/src/components/layout/root.js
@@ -8,7 +8,9 @@ const Footer = () => {
                     <a href="https:\/\/psafe.ly/vgZeqv" target="_blank" style={{color: '#6ec1e4'}}>Source Code</a>
                     <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/bugs" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Found A Bug?</a>
                     <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/abuse" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Report Abuse</a>
-                    <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/VNuFWf" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Terms of Service</a>
+                    <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/VNuFWf" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Terms of Use</a><br>
+                    <span style={{color: '#ffffff'}}>Tor hidden service: </span><a href="http:\/\/bjaxm2ek3e45ie3bohcvdjtrgsjyw72quv4dwupkst5ektwv3lie3myd.onion" id="contact" target="_blank" style={{color: '#6ec1e4'}}>bjaxm2ek3e45ie3bohcvdjtrgsjyw72quv4dwupkst5ektwv3lie3myd.onion</a>
+                    <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/torproject.org/download" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Download Tor Browser</a>
                 </p>
             </div>
         </footer>
