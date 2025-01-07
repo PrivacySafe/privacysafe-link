e5286e19042bda03a55a5f6d4d3a24408b014117
Sean O'Brien <sean@cyberclear.cc>
2025-01-07 00:49:52 -0500

    Update root.js

--- frontend/src/components/layout/root.js
+++ frontend/src/components/layout/root.js
@@ -5,10 +5,12 @@ const Footer = () => {
         <footer className="fixed bottom-0 left-0 w-full bg-[#000d1d] text-white py-2">
             <div className="container mx-auto text-center">
                 <p className="text-sm">
-                    <a href="https:\/\/psafe.ly/vgZeqv" target="_blank" style={{color: '#6ec1e4'}}>Source Code</a>
+                    <a href="https:\/\/psafe.ly/EWZ3zL" target="_blank" style={{color: '#fb521f'}}>PrivacySafe Apps</a>
+                    <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/vgZeqv" target="_blank" style={{color: '#6ec1e4'}}>Source Code</a>
                     <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/bugs" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Found A Bug?</a>
                     <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/abuse" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Report Abuse</a>
                     <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/VNuFWf" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Terms of Use</a>
+                    <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/BbQTXH" id="contact" target="_blank" style={{color: '#fb521f'}}>PrivacySafe Social</a>
                 </p>
                 <p className="text-sm">
                     <span style={{color: '#ffffff'}}>Tor Hidden Service: </span><a href="http:\/\/bjaxm2ek3e45ie3bohcvdjtrgsjyw72quv4dwupkst5ektwv3lie3myd.onion" id="contact" target="_blank" style={{color: '#6ec1e4'}}>bjaxm2ek3e45ie3bohcvdjtrgsjyw72quv4dwupkst5ektwv3lie3myd.onion</a>
