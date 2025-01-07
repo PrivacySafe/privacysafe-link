46e0444a417b3722f4d7830fd8416434181c105d
Sean O'Brien <sean@cyberclear.cc>
2025-01-07 00:25:23 -0500

    Update root.js

--- frontend/src/components/layout/root.js
+++ frontend/src/components/layout/root.js
@@ -8,8 +8,10 @@ const Footer = () => {
                     <a href="https:\/\/psafe.ly/vgZeqv" target="_blank" style={{color: '#6ec1e4'}}>Source Code</a>
                     <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/bugs" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Found A Bug?</a>
                     <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/abuse" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Report Abuse</a>
-                    <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/VNuFWf" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Terms of Use</a><br>
-                    <span style={{color: '#ffffff'}}>Tor hidden service: </span><a href="http:\/\/bjaxm2ek3e45ie3bohcvdjtrgsjyw72quv4dwupkst5ektwv3lie3myd.onion" id="contact" target="_blank" style={{color: '#6ec1e4'}}>bjaxm2ek3e45ie3bohcvdjtrgsjyw72quv4dwupkst5ektwv3lie3myd.onion</a>
+                    <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/psafe.ly/VNuFWf" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Terms of Use</a>
+                </p>
+                <p className="text-sm">
+                    <span style={{color: '#ffffff'}}>Tor Hidden Service: </span><a href="http:\/\/bjaxm2ek3e45ie3bohcvdjtrgsjyw72quv4dwupkst5ektwv3lie3myd.onion" id="contact" target="_blank" style={{color: '#6ec1e4'}}>bjaxm2ek3e45ie3bohcvdjtrgsjyw72quv4dwupkst5ektwv3lie3myd.onion</a>
                     <span style={{color: '#ffffff'}}> | </span> <a href="https:\/\/torproject.org/download" id="contact" target="_blank" style={{color: '#6ec1e4'}}>Download Tor Browser</a>
                 </p>
             </div>
